36d0c158f534cb0b87bf668d73f59c6153a8431e
Cristian Oliveira <contato@cristianoliveira.com.br>
2022-04-11 13:29:56 +0200

    test scenario

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ const port = process.env.PORT || 4000;
 
 const crypto = require('crypto');
 
+//
+//default-src 'self';
+//block-all-mixed-content; child-src https://www.google.com https://gateway-theta.sam-app.ro;
+//connect-src 'self' https://checkout.ostheta.net https://checkoutv2.ostheta.net https://checkout.sumuplink.icu/api/ https://api.notolytix.com https://o196784.ingest.sentry.io https://www.google-analytics.com https://www.googletagmanager.com cdn.ostheta.in https://gateway-theta.sam-app.ro https://api-theta.sam-app.ro;
+//font-src 'self' use.typekit.net https://fonts.googleapis.com https://fonts.gstatic.com https://checkout.ostheta.net https://cdnjs.cloudflare.com cdn.ostheta.in; frame-ancestors https://dashboard.sam-app.ro https://dashboard-theta.sam-app.ro https://*.sumup-vercel.app https://*.vercel.app https://localhost:* https://127.0.0.1:*;
+//img-src 'self' https://cdn.shoplo.com cdn.ostheta.in https://my-images.sumup.com https://catalog-images-live.s3.amazonaws.com https://catalog-images-dev.s3.amazonaws.com/ https://catalog-images-stage.s3.amazonaws.com https://cdn.sumup.store/ https://www.shopos.local.shoplonet.com cdn.ostheta.in static.sumup.com;
+//script-src 'self' ajax.googleapis.com https://www.google.com/recaptcha/api.js https://www.gstatic.com https://gateway-theta.sam-app.ro https://net-tracker.notolytix.com/main.js 'unsafe-eval' https://cdnjs.cloudflare.com https://www.google-analytics.com https://www.googletagmanager.com cdn.ostheta.in https://api-theta.sam-app.ro 'nonce-pMzT0eRwcX1GrzEyheGCVg==';
+//style-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://cdnjs.cloudflare.com https://www.google-analytics.com https://www.googletagmanager.com cdn.ostheta.in; upgrade-insecure-requests
+
 app.get("/", (req, res) => {
   const nonce = crypto.randomBytes(16).toString('base64');
   res.setHeader(
     "Content-Security-Policy",
-      " font-src 'self';" +
-      ` style-src 'self' 'nonce-${nonce}';`
-  );
+    `default-src 'self'; block-all-mixed-content; child-src https://www.google.com https://gateway-theta.sam-app.ro; connect-src 'self' https://checkout.ostheta.net https://checkoutv2.ostheta.net https://checkout.sumuplink.icu/api/ https://api.notolytix.com https://o196784.ingest.sentry.io https://www.google-analytics.com https://www.googletagmanager.com cdn.ostheta.in https://gateway-theta.sam-app.ro https://api-theta.sam-app.ro; font-src 'self' use.typekit.net https://fonts.googleapis.com https://fonts.gstatic.com https://checkout.ostheta.net https://cdnjs.cloudflare.com cdn.ostheta.in; frame-ancestors https://dashboard.sam-app.ro https://dashboard-theta.sam-app.ro https://*.sumup-vercel.app https://*.vercel.app https://localhost:* https://127.0.0.1:*; img-src 'self' https://cdn.shoplo.com cdn.ostheta.in https://my-images.sumup.com https://catalog-images-live.s3.amazonaws.com https://catalog-images-dev.s3.amazonaws.com/ https://catalog-images-stage.s3.amazonaws.com https://cdn.sumup.store/ https://www.shopos.local.shoplonet.com cdn.ostheta.in static.sumup.com; script-src 'self' ajax.googleapis.com https://www.google.com/recaptcha/api.js https://www.gstatic.com https://gateway-theta.sam-app.ro https://net-tracker.notolytix.com/main.js 'unsafe-eval' https://cdnjs.cloudflare.com https://www.google-analytics.com https://www.googletagmanager.com cdn.ostheta.in https://api-theta.sam-app.ro 'nonce-${nonce}=='; style-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://cdnjs.cloudflare.com https://www.google-analytics.com https://www.googletagmanager.com cdn.ostheta.in; upgrade-insecure-requests`);
 
   res.send(`<h1>Test CSP</h1>
     <div>Test using generated nonce: ${nonce}</div>
